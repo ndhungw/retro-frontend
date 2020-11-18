ba03c41938e9af554f58e1507f080cb951b6e0f5
Nguyen Dong Hung <ndh1379@gmail.com>
2020-11-18 16:02:07 +0700

    make popup when click user medal to be nicer

--- src/components/user/UserMedal.js
+++ src/components/user/UserMedal.js
@@ -1,13 +1,37 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
-import { Avatar } from "@material-ui/core";
+import { Avatar, Popover, Typography } from "@material-ui/core";
 import useAuth from "../../context/auth";
 import { Link } from "react-router-dom";
-// import { Redirect } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import AccountBoxIcon from "@material-ui/icons/AccountBox";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
-export default function UserMedal() {
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    margin: theme.spacing(1),
+    color: "#fff",
+  },
+  username: {
+    alignItems: "center",
+    color: "#fff",
+    margin: theme.spacing(1),
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  button: {
+    margin: theme.spacing(0, 1, 0, 1),
+  },
+  menuIcon: {
+    margin: theme.spacing(0, 2, 0, 0),
+  },
+}));
+
+export default function UserMedal({ user }) {
+  const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
   const { setAuthTokens } = useAuth();
 
@@ -32,19 +56,30 @@ export default function UserMedal() {
         onClick={handleClick}
       >
         <Avatar />
+        <Typography className={classes.username}>{user.username}</Typography>
       </Button>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
+      <Popover
         open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
         onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
       >
         <MenuItem component={Link} to={"/profile"}>
+          <AccountBoxIcon className={classes.menuIcon} />
           Profile
         </MenuItem>
-        <MenuItem onClick={handleLogout}>Logout</MenuItem>
-      </Menu>
+        <MenuItem onClick={handleLogout}>
+          {" "}
+          <ExitToAppIcon className={classes.menuIcon} /> Logout
+        </MenuItem>
+      </Popover>
     </div>
   );
 }
